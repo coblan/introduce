50186795cb8f301c23f12683f837dedfff59bf33
coblan <coblan@163.com>
2016-08-12 01:05:49 +0800

    调整配置文件

--- conf.py
+++ conf.py
@@ -331,8 +331,18 @@ texinfo_documents = [
 #
 # texinfo_no_detailmenu = False
 
-import sphinx_rtd_theme
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None)
 
-html_theme = "sphinx_rtd_theme"
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+#import sphinx_rtd_theme
+
+#html_theme = "sphinx_rtd_theme"
+
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
